512edd93dbaf33931f1ae91d7bb90a2f8e08ef5a
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-07-05 14:07:57 +0500

    +DeviceController( create/getAll methods)

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,6 +1,6 @@
 import { v4 } from 'uuid'
 import path from 'path'
-import { Device } from '../models/models.js'
+import { Device, DeviceInfo } from '../models/models.js'
 import ApiError from '../error/ApiError.js'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
@@ -11,7 +11,7 @@ const __dirname = dirname(__filename)
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body
+      let { name, price, brandId, typeId, info } = req.body
       const { image } = req.files
       let fileName = v4() + '.jpg'
       image.mv(path.resolve(__dirname, '../static', fileName))
@@ -24,6 +24,17 @@ class DeviceController {
         image: fileName,
       })
 
+      if (info) {
+        info = JSON.parse(info)
+        info.forEach((el) => {
+          DeviceInfo.create({
+            title: el.title,
+            description: el.description,
+            deviceId: device.id,
+          })
+        })
+      }
+
       return res.json(device)
     } catch (e) {
       next(ApiError.badRequest(e.message))
@@ -31,7 +42,38 @@ class DeviceController {
   }
 
   async getAll(req, res) {
-    const { brandId, typeId } = req.body
+    let { brandId, typeId, page, limit } = req.query
+    page = page || 1
+    limit = limit || 9
+    let offset = page * limit - limit
+
+    let devices
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset })
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      })
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      })
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId, brandId },
+        limit,
+        offset,
+      })
+    }
+
+    return res.json(devices)
   }
 
   async getOne(req, res) {}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import Router from 'express'
-const router = new Router()
 import deviceRouter from './deviceRouter.js'
 import typeRouter from './typeRouter.js'
 import brandRouter from './brandRouter.js'
 import userRouter from './userRouter.js'
+
+const router = new Router()
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
